5634626f6792bfb421ccea2c784cbddfb9648ad4
Saman Miran <c@samanmiran.com>
2023-02-03 22:45:36 +0100

    WikiManager has some function now

--- Assets/Scripts/WikiManager.cs
+++ Assets/Scripts/WikiManager.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using UnityEngine;
 
 // https://gamedevbeginner.com/singletons-in-unity-the-right-way/#unity_singleton
 
 public class WikiManager : MonoBehaviour
 {
+    private static string BASE_API_URL = "https://en.wikipedia.org/w/api.php";
 
     private static WikiManager _instance;
 
@@ -21,6 +25,21 @@ public class WikiManager : MonoBehaviour
         else
         {
             _instance = this;
+            // TODO just testing
+            print(GetLinksJSON("Brad Pitt", null));
         }
+    }
+
+    private string GetLinksJSON(string titles, string plcontinue)
+    {
+        string requestString = String.Format(BASE_API_URL + "?action=query&prop=links&format=json&plnamespace=0&titles={0}&pllimit=max", titles);
+        if (plcontinue != null) {
+            requestString = String.Format(requestString + "&plcontinue={0}", plcontinue);
+        }
+        HttpWebRequest request = (HttpWebRequest) WebRequest.Create(requestString);
+        HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        return jsonResponse;
     }
 }
